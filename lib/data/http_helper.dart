876b58f4ac1755772f1bcb7abab9474110abc078
Luca D'Aurizio <daurizio.lc@gmail.com>
2024-02-26 12:39:51 +0100

    first commit

---
+++ lib/data/http_helper.dart
@@ -0,0 +1,21 @@
+import 'package:globo_fitness/data/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HttpHelper {
+  // https://api.openweathermap.org/data/2.5/weather?q=London&appid=475fe39135ddda9965fb128519d32a13
+
+  final String authority = 'api.openweathermap.org';
+  final String path = 'data/2.5/weather';
+  final String apiKey = '475fe39135ddda9965fb128519d32a13';
+
+  Future<Weather> getWeather(String location) async {
+    Map<String, dynamic> parameters = {'q': location, 'appId': apiKey};
+    Uri uri = Uri.https(authority, path, parameters);
+    http.Response result = await http.get(uri);
+    Map<String, dynamic> data = json.decode(result.body);
+    Weather weather = Weather.fromJson(data);
+
+    return weather;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/bmi_screen.dart';
+import 'package:globo_fitness/screens/intro_screens.dart';
+
+void main() {
+  runApp(const GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      routes: {
+        '/': (context) => const IntroScreen(),
+        '/bmi': (context) => const BmiScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
